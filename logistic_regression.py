50446f70f8bb3a7fcc186adcd3de27811df26e4b
Sushma K N <sushmakn@Sushmas-MacBook-Pro.local>
2019-03-23 20:14:41 -0500

    Part-1 working

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,9 +2,6 @@ import numpy as np
 import json
 import math
 
-
-
-
 class NeuralNetwork:
     def __init__(self, inputFileName, epoch, learning_rate):
         self.input_file = inputFileName
@@ -23,13 +20,13 @@ class NeuralNetwork:
 
 
     def load_and_init_dataset(self):
-        dataSet = json.load(open(self.input_file))
-        self.features = np.array(dataSet["metadata"]["features"][0:-1])
-        self.dataset = np.array(dataSet["data"])
-        self.std_dataset = np.array(dataSet["data"], dtype=object)
+        dataset = json.load(open(self.input_file))
+        self.features = np.array(dataset["metadata"]["features"][0:-1])
+        self.dataset = np.array(dataset["data"])
+        self.std_dataset = np.array(dataset["data"], dtype=object)
         self.labels = self.dataset[:, -1]
         self.label_types = self.features[:, 1]
-        self.label_class = dataSet["metadata"]["features"][-1][1]
+        self.label_class = dataset["metadata"]["features"][-1][1]
         self.shape = self.dataset.shape
 
     def compute_mean_and_sd(self, data_set):
@@ -47,32 +44,17 @@ class NeuralNetwork:
             else:
                 categorical_indices.append(index)
 
-
-        catrgorical_dataset = np.zeros((dataset_obj.shape[0], len(categorical_indices)))
         if (len(categorical_indices)):
-
-            #self.std_dataset = np.delete(self.std_dataset, categorical_indices, axis=1)
-            data = np.empty_like(self.std_dataset)
             for index in categorical_indices:
-                dummy = []
-                #self.std_dataset = np.insert(self.std_dataset, (index), [999], axis=1)
                 for row_index, row in enumerate(self.std_dataset):
-                    print(np.isin(self.features[index][1], self.dataset[row_index, 1]).astype(int).tolist())
-                    print(self.std_dataset[row_index, index])
-                    data[row_index, index] = np.array(np.isin(self.features[index][1], self.dataset[row_index, index]).astype(int).tolist())
-                    dummy.append([1, 2, 3])
-                dummy = np.array(dummy)
-                self.std_dataset = np.hstack((dummy, self.std_dataset))
-                print()
-            trainMatrix = self.dataset[:, categorical_indices]
-            testMatrix = dataset_obj.dataset[:, categorical_indices]
+                    self.std_dataset[row_index, index] = np.isin(self.features[index][1], self.dataset[row_index, index]).astype(int).tolist()
+                for row_index, row in enumerate(dataset_obj.std_dataset):
+                    dataset_obj.std_dataset[row_index, index] = np.isin(self.features[index][1], dataset_obj.dataset[row_index, index]).astype(int).tolist()
 
         if (len(numeric_indices)):
             self.compute_mean_and_sd(self.std_dataset[:, numeric_indices].astype(float))
             self.std_dataset[:,numeric_indices] = (self.dataset[:,numeric_indices].astype(float) - self.mean) / self.sd
             dataset_obj.std_dataset[:,numeric_indices] = (dataset_obj.dataset[:,numeric_indices].astype(float) - self.mean) / self.sd
-
-
 
         return dataset_obj
 
@@ -91,22 +73,20 @@ class NeuralNetwork:
 
         cross_entropy_error = 0.0
         corrects = 0
-        #label_list = self.get_binary_label_list(self.std_dataset[:,-1])
         for index, row in enumerate(self.std_dataset):
             if (row[-1] == self.label_class[1]):
                 y = 1
             else:
                 y = 0
-            row_attributes = row[0: -1].astype(float)
-            neural_output = self.find_neural_net_output(self.weights, self.flatten(row_attributes))
+            row_attributes = self.flatten(row[0:-1])
+            #row_attributes = row[0: -1].astype(float)
+            neural_output = self.find_neural_net_output(self.weights, row_attributes)
             sigmoid_activation_value = 1.0 / (1 + math.exp(-neural_output))
             if ((sigmoid_activation_value >= 0.5 and y==1) or  (sigmoid_activation_value < 0.5 and y==0)):
                 corrects += 1
             cross_entropy_error += (-1.0 * y * math.log(sigmoid_activation_value)) - ((1.0-y) * math.log((1-sigmoid_activation_value)))
             for index in range(len(row_attributes)):
-                gradient =  (sigmoid_activation_value - y)
-                if (self.label_types[index-1] == "numeric"):
-                    gradient *= row_attributes[index]
+                gradient =  (sigmoid_activation_value - y) * row_attributes[index]
                 self.weights[index] += -1.0 * self.learning_rate * gradient
         print("{0} {1:.12f} {2} {3}".format(epoch, cross_entropy_error, corrects, (self.shape[0]-corrects)))
 
@@ -118,7 +98,8 @@ class NeuralNetwork:
 
         label_list = self.get_binary_label_list(test_obj.std_dataset[:, -1])
         for index,row in enumerate(test_obj.std_dataset):
-            row_attributes = row[0: -1].astype(float)
+            #row_attributes = row[0: -1].astype(float)
+            row_attributes = self.flatten(row[0:-1])
             neural_output = self.find_neural_net_output(self.weights, self.flatten(row_attributes))
             sigmoid_activation_value = 1.0 / (1 + math.exp(-neural_output))
             actual_class = label_list[index]
@@ -163,7 +144,7 @@ class NeuralNetwork:
         newlist = []
         for item in irregular_list:
             if isinstance(item, list):
-                newlist = newlist + flatten(item)
+                newlist = newlist + self.flatten(item)
             else:
                 newlist.append(item)
         return newlist
@@ -171,14 +152,14 @@ class NeuralNetwork:
 
 if __name__ == '__main__':
     np.random.seed(0)
-    # train_neural = NeuralNetwork("./Resources/banknote_train.json", 10, 0.01)
-    # test_neural = NeuralNetwork("./Resources/banknote_test.json", 10, 0.01)
+    train_neural = NeuralNetwork("./Resources/banknote_train.json", 10, 0.01)
+    test_neural = NeuralNetwork("./Resources/banknote_test.json", 10, 0.01)
 
     # train_neural = NeuralNetwork("./Resources/magic_train.json", 10, 0.01)
     # test_neural = NeuralNetwork("./Resources/magic_test.json", 10, 0.01)
 
-    train_neural = NeuralNetwork("./Resources/heart_train.json", 10, 0.01)
-    test_neural = NeuralNetwork("./Resources/heart_test.json", 10, 0.01)
+    # train_neural = NeuralNetwork("./Resources/heart_train.json", 20, 0.05)
+    # test_neural = NeuralNetwork("./Resources/heart_test.json", 20, 0.05)
 
     train_neural.load_and_init_dataset()
     test_neural.load_and_init_dataset()
